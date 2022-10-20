8c8546ebca46346f84cb622f48ca819cf9233d36
raythurman2386 <raymondthurman5@gmail.com>
2022-10-20 10:22:06 -0500

    initial setup complete

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next Ecommerce</title>
+        <meta
+          name="description"
+          content="Ecommerce application built with NextJS and Sanity"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  );
+}
